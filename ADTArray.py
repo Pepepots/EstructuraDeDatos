1561e0f2037b04aa9bf435bca7f7bfe49a33797b
Jose Luis <luizg9414@gmail.com>
2021-09-10 10:47:44 -0500

    creacion de metodo clear

---
+++ ADTArray.py
@@ -0,0 +1,20 @@
+class Array():
+    def __init__( self, tamano ):
+        self.__tamano = tamano
+        self.__data = [0 for x in range(self.__tamano)]
+
+    def get_item( self, index ):
+        return self.__data[ index ]
+
+    def to_string( self ):
+        print( self.__data )
+
+    def clear ( self, dato ):
+        self.__data = [dato for x in range(self.__tamano)]
+
+ejemplo = Array(5)
+
+ejemplo.to_string()
+ejemplo.get_item(3)
+ejemplo.clear(1)
+ejemplo.to_string()
