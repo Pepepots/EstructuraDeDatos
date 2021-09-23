81267b7816e02289eddb8460acf73599aa1bc8c4
Jose Luis <luizg9414@gmail.com>
2021-09-23 00:39:26 -0500

    Creacion metodo clear

--- ADT2d.py
+++ ADT2d.py
@@ -31,6 +31,10 @@ class ADTArray2D():
     def get_col_size( self ):
         return self.__col
 
+    def clear( self, dato ):
+        for i in range( len(self.__data) ):
+            self.__data[i] = [dato for x in range(self.__col)]
+
 
 arr = ADTArray2D( 3, 4 )
 
@@ -44,3 +48,10 @@ print( arr.get_item( 2, 2 ) )
 
 pepe = arr.get_item( 0, 0 )
 print( pepe.get_nombre() )
+
+arr.clear( 1 )
+arr.to_string()
+
+# arr.clear( Persona( 'Pepe' ) )
+arr.clear( None )
+arr.to_string()
