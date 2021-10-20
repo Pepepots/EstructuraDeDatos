4318b226371d308b003390f7967f05530daa854a
Jose Luis <luizg9414@gmail.com>
2021-10-20 16:14:25 -0500

    lista doble ligada Java

---
+++ Java/ListasLigadas/src/ico/fes/edd1358/LinkedListADT.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.edd1358;
+
+/**
+ *
+ * @author jose
+ */
+public class LinkedListADT {
+    
+//   private class Nodo {
+//    
+//       
+//       
+//   } 
+    
+}
---
+++ Java/ListasLigadas/src/listasLigadas1358/ListasLigadas1358.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listasLigadas1358;
+
+/**
+ *
+ * @author jose
+ */
+public class ListasLigadas1358 {
+    public static void main(String[] args) {
+        
+    }
+}
