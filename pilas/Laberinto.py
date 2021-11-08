313bf2e6caa48ed02c19f68fd0da45dcf9f01d30
Jose Luis <luizg9414@gmail.com>
2021-11-08 15:35:04 -0600

    Codigo limpio laberinto

--- pilas/Laberinto.py
+++ pilas/Laberinto.py
@@ -9,14 +9,9 @@ class Laberinto:
         for r in range(self.row):
             for c in range(self.col):
                 self.tablero.set_item( r, c, self.info[r][c] )
-        # self.tablero.clear( 'P' )
-        # self.tablero.set_item( entrada[ 0 ], entrada[ 1 ], 'E' )
-        # self.tablero.set_item( salida[ 0 ], salida[ 1 ], 'S' )
-        # for par in pasillos:
-        #     self.tablero.set_item( par[ 0 ], par[ 1 ], 'C' )
+
         self.respuesta = StackADT()
         self.buscarEntrada()
-        self.respuesta.push(self.entrada)
 
     def buscarEntrada( self ):
         for r in range(self.row):
@@ -24,7 +19,7 @@ class Laberinto:
                 tmp = self.tablero.get_item( r, c )
                 if tmp == 'E':
                     self.entrada = [ r, c ]
-
+        self.respuesta.push(self.entrada)
 
     def read(self,ruta):
         txt = open(ruta,'rt')
@@ -77,6 +72,8 @@ class Laberinto:
         estadoBot = None
         estadoRigth = None
 
+        # Pregunta si es limite para definir su estado osea si es pared o no 
+
         if self.esLimite(top):
             estadoTop = self.tablero.get_item( top[0], top[1])
         
@@ -89,8 +86,9 @@ class Laberinto:
         if self.esLimite(rigth):
             estadoRigth = self.tablero.get_item( rigth[0], rigth[1])
 
+        # Hacemos la pregunta de donde avanzar si no puede avanzar en ningun lado se regresa una posicion 
+
         if estadoTop == 'C' or estadoTop == 'S':
-            # print('Si estoy en top')
             if estadoTop == 'S':
                 self.respuesta.push(top)
                 return
@@ -122,107 +120,38 @@ class Laberinto:
         else:
             self.tablero.set_item( cor[ 0 ], cor[ 1 ], 'X')
             self.respuesta.pop()
-
-        # print(top)
-        # if self.esLimite( top[0], top[1] ):
-        #     # print('hoil')
-        #     estado = self.tablero.get_item( top[0], top[1] )
-        #     if estado == 'S':
-        #         self.respuesta.push(top)
-        #         return
-        #     if estado == 'C':
-        #         self.tablero.set_item(top[ 0 ], top[ 1 ], 'V')
-        #         self.respuesta.push(top)
-        #         # self.encontrarCamino(estado)
-        #     elif self.esLimite( left[0], left[1] ) :
-        #         estado = self.tablero.get_item( left[0], left[1] )
-        #         if estado == 'S':
-        #             self.respuesta.push(left)
-        #             return
-        #         if estado == 'C':
-        #             self.tablero.set_item(left[ 0 ], left[ 1 ], 'V')
-        #             self.respuesta.push(left)
-        #             # self.encontrarCamino(estado)
-        #         elif self.esLimite( bot[0], bot[1] ):
-        #             estado = self.tablero.get_item( bot[0], bot[1] )
-        #             if estado == 'S':
-        #                 self.respuesta.push(bot)
-        #                 # print('llegue a la solucion')
-        #                 return
-        #             if estado == 'C':
-        #                 self.tablero.set_item(bot[ 0 ], bot[ 1 ], 'V')
-        #                 self.respuesta.push(bot)
-        #                 # self.encontrarCamino(estado)
-        #             elif self.esLimite( rigth[0], rigth[1] ):
-        #                 print(rigth)
-        #                 estado = self.tablero.get_item( rigth[0], rigth[1] )
-        #                 if estado == 'S':
-        #                     self.respuesta.push(rigth)
-        #                     return
-        #                 if estado == 'C':
-        #                     self.tablero.set_item(rigth[ 0 ], rigth[ 1 ], 'V')
-        #                     self.respuesta.push(rigth)
-        #                     # self.encontrarCamino(estado)
-        #                 else:
-        #                     self.tablero.set_item( cor[ 0 ], cor[ 1 ], 'X')
-        #                     self.respuesta.pop()
+        
         # self.formato()
         self.encontrarCamino()
-        
 
-        # print(cor)
-        # print(estado)
-        # print(self.tablero.get_item( cor[0]-1, cor[1]))
-        # while estado != 'S':
-        # if self.tablero.get_item( cor[0]-1, cor[1] ) != 'P' and self.tablero.get_item( cor[0]-1, cor[1] ) != 'S':
-        #     self.tablero.set_item( cor[0]-1, cor[1], 'V' )
-        #     self.respuesta.push([ cor[0]-1, cor[1]])
-    
     def getRespuesta( self ):
         self.respuesta.toString()
     
 
-# entrada = [ 9, 1 ]
-# salida = [1,9]
-# camino = [(8,1),(8,2),(8,3),(7,3),(6,3),(5,3),(5,2),(4,2),(3,2),(3,1),(3,2),(3,1),(3,1),(3,0),(3,3),(3,4),(3,5),(3,6),(2,5),(1,5),(1,6),(1,7),(8,4),(1,8)]
-
 prueba1 = Laberinto('entrada.txt')
 prueba1.formato()
 prueba1.encontrarCamino()
-prueba1.getRespuesta()
 prueba1.formato()
+prueba1.getRespuesta()
 
-print('------')
+print('------------------------------------------------------')
 prueba2 = Laberinto('entrada2.txt')
 prueba2.formato()
 prueba2.encontrarCamino()
-prueba2.getRespuesta()
 prueba2.formato()
+prueba2.getRespuesta()
 
-print('------')
+print('------------------------------------------------------')
 prueba3 = Laberinto('prueba3.txt')
 prueba3.formato()
 prueba3.encontrarCamino()
-prueba3.getRespuesta()
 prueba3.formato()
+prueba3.getRespuesta()
 
+print('------------------------------------------------------')
+prueba4 = Laberinto('rebuscado.txt')
+prueba4.formato()
+prueba4.encontrarCamino()
+prueba4.formato()
+prueba4.getRespuesta()
 
-# txt = open('entrada.txt','rt')
-# row = int(txt.readline())
-# col = int(txt.readline())
-# info = txt.readlines()
-
-# for index in range( len(info) ):
-#     info[index] = info[index].rstrip('\n').split(',')
-
-# tablero = ADTArray2D( row, col )
-# for r in range(row):
-#     for c in range(col):
-#             tablero.set_item( r, c, info[r][c] )
-        # print( f'({r},{c})')
-        
-# tablero.to_string()
-
-# print(row)
-# print(col)
-# print(info)
